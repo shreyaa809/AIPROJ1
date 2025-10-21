9cec26109dca95ed3286c6538eaac0d8a379d9cc
Shreya Shukla <shreyashukla218@gmail.com>
2025-10-21 21:04:48 +0530

    ready

--- training.py
+++ training.py
@@ -74,6 +74,6 @@ sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5,verbose=1)
-model.save('chatbot_model.model')
+hist=model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5,verbose=1)
+model.save('chatbotmodel.h5',hist)
 print("Done!")
